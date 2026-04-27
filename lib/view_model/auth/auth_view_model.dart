5a9dc96fba7c8b3557ea23180a1bf1fca5253532
Jimesh Desai <125181035+jimeshdesai@users.noreply.github.com>
2026-04-27 11:13:26 +0530

    add api call for get and post without view model

--- lib/view_model/auth/auth_view_model.dart
+++ lib/view_model/auth/auth_view_model.dart
@@ -109,4 +109,137 @@ class AuthViewModel with ChangeNotifier {
     });
     notifyListeners();
   }
+
+  //Login api
+
+  // User1 for Login user
+  AuthLoginModel? user1;
+
+  // getting register user data
+  AuthLoginModel? get getuser1 => user1;
+
+  void loginapi(String email, String pass, BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    String fcmtoken = prefs.getString('fcmtoken') ?? '';
+    final body = {
+      'email': email,
+      'password': pass,
+      'device_token': fcmtoken,
+    };
+    print(body);
+    dialog(context);
+    final response = await http.post(
+      Uri.parse('${Constant.apicall}api/v1/user/login'),
+      body: body,
+    );
+    print(response.request);
+    print(response.statusCode);
+    Navigator.pop(context);
+    try {
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        print(body);
+        print('Login: ${data}');
+        print('Response Body: ${data['success']}');
+        if (data['success'] == true) {
+          token = data['access_token'];
+          user1 = AuthLoginModel.fromJson(data);
+          Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => EntryPoint(
+                        currentIndex: 0,
+                      )),
+                      (route) => false);
+          notifyListeners();
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Invalid Input Error')));
+          notifyListeners();
+        }
+      } else if (response.statusCode == 400) {
+        var data = jsonDecode(response.body.toString());
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(data['message'])));
+        notifyListeners();
+      } else if (response.statusCode == 500) {
+        var data = jsonDecode(response.body.toString());
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(data['message'])));
+        notifyListeners();
+      } else {
+        var data = jsonDecode(response.body.toString());
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(data['message'])));
+        notifyListeners();
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Something went Wrong')));
+      notifyListeners();
+    }
+  }
+
+  GetChatModel? detail;
+
+  //getting notigication detail page
+  GetChatModel? get getdetail => detail;
+
+  bool get getload => _isLoading;
+
+  bool _isLoading = true;
+
+  void addLoad(bool isLoading) {
+    _isLoading = isLoading;
+    notifyListeners();
+  }
+
+  Future<GetChatModel?> getchatapi(BuildContext context) async {
+    addLoad(true);
+    final prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString('token') ?? '';
+    var header = {'authorization': 'Bearer $token'};
+    final response = await http.get(
+        Uri.parse('${Constant.apicall}api/v1/chat/users'),
+        headers: header);
+    print(response.request);
+    print(response.statusCode);
+    try {
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+       if (data['success'] == true) {
+          try {
+            detail = GetChatModel.fromJson(data);
+          } catch (e) {
+            print('error:$e');
+          }
+          addLoad(false);
+          notifyListeners();
+          return detail;
+        } else {
+          addLoad(false);
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Invalid Input Error')));
+          notifyListeners();
+        }
+      } else if (response.statusCode == 409) {
+        addLoad(false);
+        var data = jsonDecode(response.body.toString());
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(data['message'])));
+        notifyListeners();
+      } else {
+        addLoad(false);
+        var data = jsonDecode(response.body.toString());
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(data['message'])));
+        notifyListeners();
+      }
+    } catch (e) {
+      addLoad(false);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Something went Wrong')));
+      notifyListeners();
+    }
+  }
 }
